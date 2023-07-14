f5a6ad5f1c380d42e2a938d8dfe8a4a05811e8f2
debangan03 <debangan2019@.com>
2023-07-14 21:21:37 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
+import Image from "next/image";
 import Weathercomp from "./Weathercomp";
+import bg from './bg.jpg'
 
 export default async function Home() {
   return (
-    <main
-      className="bg-cover bg-center h-screen w-screen"
-      style={{
-        backgroundImage: "url('./bg.jpg')",
-      }}
-    >
+    <main className="bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600 bg-cover w-screen h-screen">
+      
       <Weathercomp />
       <div className=" backdrop-blur-sm bg-black/30 fixed bottom-0 w-screen p-4 text-center text-cyan-200">
         © 2023 Copyright:
